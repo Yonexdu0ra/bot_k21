e157a1096451140eb063cc7304766e012905ac17
yonexdu0ra <phamngocquy.7303@gmail.com>
2024-03-29 20:51:30 +0700

    update lại chức năng xem lịch học ictu

--- src/controllers/command/getLichHocICTU.js
+++ src/controllers/command/getLichHocICTU.js
@@ -24,12 +24,11 @@ async function getLichHocICTU(msg, match) {
       });
       return;
     }
-    // const { deleteMessage } = await typingMessage(this, {
-    //   chat_id,
-    //   message: `Đợi chút nhé...\n\n(sau 18h chủ nhật hàng tuần lịch sẽ là của tuần kế tiếp nhé ^^) `,
-    // });
-    await this.sendMessage(chat_id, "Chức nay này hiện tại đang tạm dừng");
-    return
+    const { editMessage } = await typingMessage(this, {
+      chat_id,
+      message: `Đợi tý để mình xác thực tài khoản Đăng ký tin chỉ nhé ^^`,
+    });
+
     const browser = await puppeteer.launch(browerConfig);
     const page = await browser.newPage();
     page.on("dialog", async (dialog) => {
@@ -44,9 +43,14 @@ async function getLichHocICTU(msg, match) {
         reply_to_message_id: message_id,
       });
       await browser.close();
-      await deleteMessage();
+      await editMessage(
+        "Hmm... Có vẻ như *Tài khoản* hoặc *Mật khẩu* không chính xác"
+      );
       return;
     }
+    await editMessage(
+      "Xác thức tài khoản thành công ^^ \nLưu ý: *sau 18 giờ hàng ngày lịch học sẽ là lịch học của ngày hôm sau*"
+    );
     await page.goto(
       "http://220.231.119.171/kcntt/(S(33uxr0lc44m242fbvo0zubml))/Reports/Form/StudentTimeTable.aspx"
     );
@@ -54,184 +58,127 @@ async function getLichHocICTU(msg, match) {
       return [...document.querySelectorAll(".cssListItem")][0] ? true : false;
     });
     if (!isLich) {
-      await selectSemester(page, 2);
+      const isSelectId = await selectSemester(page);
+      await selectSemester(page, 5);
       await page.waitForNavigation();
-      await selectSemester(page, 1);
+      await selectSemester(page, isSelectId);
       await page.waitForNavigation();
     }
+
     const data = await page.evaluate(() => {
       // crawl dữ liệu table trong dktc
-      const arr = [];
-      const table = document.querySelector("table#gridRegistered");
-      const [, ...body] = [...table.querySelectorAll("tr")];
-      body.pop();
-      body.forEach((tr) => {
-        var _listTd$, _listTd$2, _listTd$3, _listTd$4, _listTd$5, _listTd$6;
-        let obj = {};
-        const listTd = [...tr.children];
-        const lopHocPhan =
-          (_listTd$ = listTd[1]) === null || _listTd$ === void 0
-            ? void 0
-            : _listTd$.innerText;
-        const hocPhan =
-          (_listTd$2 = listTd[2]) === null || _listTd$2 === void 0
-            ? void 0
-            : _listTd$2.innerText;
-        let thoiGian =
-          (_listTd$3 = listTd[3]) === null || _listTd$3 === void 0
-            ? void 0
-            : _listTd$3.innerText;
-        const diaDiem =
-          (_listTd$4 = listTd[4]) === null || _listTd$4 === void 0
-            ? void 0
-            : _listTd$4.innerText;
-        const giangVien =
-          (_listTd$5 = listTd[5]) === null || _listTd$5 === void 0
-            ? void 0
-            : _listTd$5.innerText;
-        const soTC =
-          (_listTd$6 = listTd[8]) === null || _listTd$6 === void 0
-            ? void 0
-            : _listTd$6.innerText;
-        const scheduleEntries =
-          thoiGian === null || thoiGian === void 0
-            ? void 0
-            : thoiGian.split("\n");
-        obj["lopHocPhan"] = lopHocPhan;
-        obj["hocPhan"] = hocPhan;
-        obj["giangVien"] = giangVien;
-        obj["soTC"] = soTC;
-        obj["lichHoc"] = {};
-        let tuan;
-        for (const entry of scheduleEntries) {
-          if (entry.trim() === "") {
-            // Bỏ qua các dòng trống
-            continue;
-          } else if (entry.startsWith("Từ ")) {
-            const [, startDate, , endDate, date] = entry.split(" ");
-            tuan = date;
-            obj["lichHoc"][tuan] = {
-              startDate,
-              endDate: endDate.split(":")[0],
-            };
-            if (!("date" in obj["lichHoc"][tuan])) {
-              obj["lichHoc"][tuan]["date"] = {};
-            }
-          } else {
-            var _entry$trim;
-            let [thu, tiet] =
-              entry === null ||
-              entry === void 0 ||
-              (_entry$trim = entry.trim()) === null ||
-              _entry$trim === void 0
-                ? void 0
-                : _entry$trim.split("tiết");
-            obj["lichHoc"][tuan]["date"][
-              thu === null || thu === void 0 ? void 0 : thu.trim()
-            ] = tiet === null || tiet === void 0 ? void 0 : tiet.trim();
-          }
-        }
-        let text = diaDiem.split("\n").join(" ");
-        if (text.includes("(")) {
-          while (text.includes("(")) {
-            let index = text.indexOf("(");
-            let index2 = text.indexOf("(", text.indexOf("(") + 1);
-            let data = text.slice(index, index2 === -1 ? text.length : index2);
-            text = text.slice(index2);
-            let [tuan, ...phong] = data.split(" ");
-            phong = phong.join(" ");
-            tuan = tuan.replace("(", "").replace(")", "").trim().split(",");
-            tuan.forEach((x) => {
-              obj.lichHoc[`(${x})`]["diaDiem"] = phong;
-            });
-          }
-        } else {
-          for (let tuanHoc in obj.lichHoc) {
-            if (!obj.lichHoc[tuanHoc]["diaDiem"]) {
-              obj.lichHoc[tuanHoc]["diaDiem"] = text;
-            }
-          }
-        }
-        arr.push(obj);
-      });
-      return arr;
-    });
-    await browser.close();
-    const array = [];
-    let today = convertDateUTC();
-    const convertDate = (dateString) => {
-      const parts = dateString.split("/");
-      const formattedDate = `${parts[1]}/${parts[0]}/${parts[2]}`;
-      return formattedDate;
-    };
-    let text = "";
-    // kiểm tra nếu đây là chủ nhật và sau khi hét giờ học thì sẽ lấy ra lịch của tuần sau
-    if (today.getDay() == 0 && today.getHours() >= 18) {
-      await deleteMessage();
-      text += "Đây là lịch học tuần sau: \n\n";
-      today.setDate(today.getDate() + 1);
-    }
-    data.forEach((obj) => {
-      let newObj = {
-        ...obj,
-        lichHoc: {},
-      };
-      for (let lich in obj.lichHoc) {
-        const dateStart = new Date(convertDate(obj.lichHoc[lich].startDate));
-        const dateEnd = new Date(convertDate(obj.lichHoc[lich].endDate));
-        if (dateStart <= today && dateEnd >= today) {
-          newObj.lichHoc = obj.lichHoc[lich].date;
-          newObj["diaDiem"] = obj.lichHoc[lich].diaDiem;
-          array.push(newObj);
+      const table = document.querySelector("#gridRegistered");
+
+      const dataLesson = [];
+      if (table) {
+        const tbody = [...table.children[0].children];
+        // slice header table
+        tbody.shift();
+        // slice footer table
+        tbody.pop();
+        for (const lessonData of tbody) {
+          const listTd = [...lessonData.children];
+          let i = listTd.length == 11 ? 1 : 2;
+          const obj = {
+            class_name: listTd[i++].innerText || "",
+            class_code: listTd[i++].innerText || "",
+            time: listTd[i++].innerText || "",
+            address: (listTd[i++].innerText || "").replace(/[\n\t]/g, " "),
+            teacher_name: listTd[i++].innerText || "",
+            number_of_student: listTd[i++].innerText || "",
+            number_of_student_register: listTd[i++].innerText || "",
+            credits: listTd[i++].innerText || "",
+            tuition: listTd[i++].innerText || "",
+            note: listTd[i++].innerText || "",
+          };
+          dataLesson.push(obj);
         }
       }
+      return dataLesson;
     });
-    let thuTrongTuan = {
-      "Thứ 2": 2,
-      "Thứ 3": 3,
-      "Thứ 4": 4,
-      "Thứ 5": 5,
-      "Thứ 6": 6,
-      "Thứ 7": 7,
-      "Chủ nhật": 8,
-    };
+    await browser.close();
 
-    //sắp xếp lịch
-    array.sort((a, b) => {
-      let thuA = Object.keys(a.lichHoc)[0];
-      let thuB = Object.keys(b.lichHoc)[0];
-      return thuTrongTuan[thuA] - thuTrongTuan[thuB];
-    });
-    if (data.length < 1) {
-      await deleteMessage();
-      await this.sendMessage(chat_id, "Tuần này bạn không có lịch học 🥰", {
-        reply_to_message_id: message_id,
-      });
-      return;
+    function extractNumbersToArray(str) {
+      const array = [];
+      if (!str) {
+        return [];
+      }
+      for (let index = 0; index < str.length; index++) {
+        const element = str[index];
+        if (!isNaN(parseInt(element))) {
+          array.push(parseInt(element));
+        }
+      }
+      return array;
     }
-    for (let obj of array) {
-      text += `Môn Học: <strong>${
-        obj.lopHocPhan
-      }</strong>\nGiảng viên: <strong>${
-        obj.giangVien
-      }</strong>\nLịch Học: <strong>${
-        JSON.stringify(obj.lichHoc).replace(/[{}]/g, "").replace(/"/g, " ") ||
-        "không xác định"
-      }</strong>\nĐịa Điểm: <strong>${
-        obj.diaDiem || "không xác định"
-      }</strong>\n\n`;
+
+    function getTime(time) {
+      if (!time) {
+        return "Hiện không có lịch 🎉✨";
+      }
+      const dataTime = time.split("Từ");
+      const today = new Date();
+      if (today.getHours() >= 18) {
+        today.setHours(5);
+        today.setDate(today.getDate() + 1);
+      }
+      for (const x of dataTime) {
+        if (x) {
+          const [timeStartAndEnd, date] = x.split("\n");
+          const [, tuan] = timeStartAndEnd.split(":");
+          const timeStart = timeStartAndEnd
+            .split(" đến ")[0]
+            .split("/")
+            .reverse()
+            .join("-");
+          const timeEnd = timeStartAndEnd
+            .split(":")[0]
+            .split(" đến ")[1]
+            .split("/")
+            .reverse()
+            .join("-");
+
+          const dateStart = new Date(timeStart);
+          const dateEnd = new Date(timeEnd);
+
+          dateEnd.setHours(18);
+          dateEnd.setMinutes(0);
+          dateEnd.setSeconds(0);
+          if (dateStart <= today && today <= dateEnd) {
+            return `(${extractNumbersToArray(tuan).join(",")}) ${date.trim()}`;
+          }
+        }
+      }
+      return "Hiện không có Lịch 🎉✨";
     }
-    await deleteMessage();
-    await this.sendMessage(chat_id, text, {
-      parse_mode: "HTML",
-      disable_web_page_preview: true,
-      reply_to_message_id: message_id,
+
+    const newDataConvert = data.map((x) => {
+      return {
+        ...x,
+        time: getTime(x.time),
+      };
     });
+    await editMessage(
+      "Đây là lịch tuần này của bạn (*Mọi thông tin đều được lấy ở Đăng ký tín chỉ*): "
+    );
+    for (const iterator of newDataConvert) {
+      // if (iterator.time === "Hiện không có Lịch 🎉✨") {
+      //   continue;
+      // }
+      await this.sendMessage(
+        chat_id,
+        `\`\`\`json\n${JSON.stringify(iterator, null, 2)}\`\`\``,
+        {
+          parse_mode: "Markdown",
+        }
+      );
+    }
   } catch (error) {
     console.error(error);
     await this.sendMessage(chat_id, `Huhu lỗi rồi thử lại sau ít phút nhé`, {
       reply_to_message_id: message_id,
     });
+    return;
   }
 }
 export default getLichHocICTU;
--- src/util/selectSemester.js
+++ src/util/selectSemester.js
@@ -3,6 +3,15 @@ async function selectSemester(page, hocKy) {
     if (!page) {
       return;
     }
+    if (!hocKy) {
+      const selectId = await page.evaluate(() => {
+        const listSelect = [...document.querySelectorAll("select")];
+        if (listSelect) {
+          return listSelect[1].selectedIndex;
+        }
+      });
+      return selectId;
+    }
     await page.evaluate((hocKy) => {
       const listSelect = [...document.querySelectorAll("select")];
       if (listSelect[1]) {
