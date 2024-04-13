332f1b31df979fcca4e48f0d666866ff663795f4
yonexdu0ra <phamngocquy.7303@gmail.com>
2024-04-13 23:39:00 +0700

    fix lỗi không tương thích brower ở command diemthi

--- src/controllers/callback_query/getDiemThi.js
+++ src/controllers/callback_query/getDiemThi.js
@@ -46,42 +46,107 @@ async function getDiemThiICTU({ data, message }) {
     // if (json.index != selectoHocKy) {
     // }
     await page.waitForNavigation();
-    const listDiemThiData = await page.evaluate(() => {
-      const table = document.querySelector("#tblStudentMark");
-      if (!table) {
-        return [];
-      }
-      const tbody = table.children[0];
-      if (!tbody) {
-        return [];
-      }
-      const list = [...tbody.children];
-      list.shift();
-      list.pop();
-      if (list.length === 0) {
-        return [];
-      }
-      const data = list.map((tr) => {
-        const tds = tr.children;
-        return {
-          // stt: tds[0].textContent,
-          course_code: tds[1].textContent?.trim() || "Hiện chưa có",
-          module: tds[2].textContent?.trim() || "Hiện chưa có",
-          credit: tds[3].textContent?.trim() || "Hiện chưa có",
-          number_of_times_retaken: tds[4].textContent?.trim() || "Hiện chưa có",
-          number_of_exam_attempts: tds[5].textContent?.trim() || "Hiện chưa có",
-          ordinal_score: tds[6].textContent?.trim() || "Hiện chưa có",
-          overall_subject_grade: tds[7].textContent?.trim() || "Hiện chưa có",
-          evaluation: tds[8].textContent?.trim() || "Hiện chưa có",
-          student_code: tds[9].textContent?.trim() || "Hiện chưa có",
-          cc: tds[10].textContent?.trim() || "Hiện chưa có",
-          score: tds[11].textContent?.trim() || "Hiện chưa có",
-          total: tds[12].textContent?.trim() || "Hiện chưa có",
-          letter_grades: tds[13].textContent?.trim() || "Hiện chưa có",
-        };
-      });
-      return data;
-    });
+   const listDiemThiData = await page.evaluate(() => {
+     const table = document.querySelector("#tblStudentMark");
+     if (!table) {
+       return [];
+     }
+     const tbody = table.children[0];
+     if (!tbody) {
+       return [];
+     }
+     const list = [...tbody.children];
+     list.shift();
+     list.pop();
+     if (list.length === 0) {
+       return [];
+     }
+     const data = list.map((tr) => {
+       var _tds$1$textContent,
+         _tds$2$textContent,
+         _tds$3$textContent,
+         _tds$4$textContent,
+         _tds$5$textContent,
+         _tds$6$textContent,
+         _tds$7$textContent,
+         _tds$8$textContent,
+         _tds$9$textContent,
+         _tds$10$textContent,
+         _tds$11$textContent,
+         _tds$12$textContent,
+         _tds$13$textContent;
+       const tds = tr.children;
+       return {
+         // stt: tds[0].textContent,
+         course_code:
+           ((_tds$1$textContent = tds[1].textContent) === null ||
+           _tds$1$textContent === void 0
+             ? void 0
+             : _tds$1$textContent.trim()) || "Hiện chưa có",
+         module:
+           ((_tds$2$textContent = tds[2].textContent) === null ||
+           _tds$2$textContent === void 0
+             ? void 0
+             : _tds$2$textContent.trim()) || "Hiện chưa có",
+         credit:
+           ((_tds$3$textContent = tds[3].textContent) === null ||
+           _tds$3$textContent === void 0
+             ? void 0
+             : _tds$3$textContent.trim()) || "Hiện chưa có",
+         number_of_times_retaken:
+           ((_tds$4$textContent = tds[4].textContent) === null ||
+           _tds$4$textContent === void 0
+             ? void 0
+             : _tds$4$textContent.trim()) || "Hiện chưa có",
+         number_of_exam_attempts:
+           ((_tds$5$textContent = tds[5].textContent) === null ||
+           _tds$5$textContent === void 0
+             ? void 0
+             : _tds$5$textContent.trim()) || "Hiện chưa có",
+         ordinal_score:
+           ((_tds$6$textContent = tds[6].textContent) === null ||
+           _tds$6$textContent === void 0
+             ? void 0
+             : _tds$6$textContent.trim()) || "Hiện chưa có",
+         overall_subject_grade:
+           ((_tds$7$textContent = tds[7].textContent) === null ||
+           _tds$7$textContent === void 0
+             ? void 0
+             : _tds$7$textContent.trim()) || "Hiện chưa có",
+         evaluation:
+           ((_tds$8$textContent = tds[8].textContent) === null ||
+           _tds$8$textContent === void 0
+             ? void 0
+             : _tds$8$textContent.trim()) || "Hiện chưa có",
+         student_code:
+           ((_tds$9$textContent = tds[9].textContent) === null ||
+           _tds$9$textContent === void 0
+             ? void 0
+             : _tds$9$textContent.trim()) || "Hiện chưa có",
+         cc:
+           ((_tds$10$textContent = tds[10].textContent) === null ||
+           _tds$10$textContent === void 0
+             ? void 0
+             : _tds$10$textContent.trim()) || "Hiện chưa có",
+         score:
+           ((_tds$11$textContent = tds[11].textContent) === null ||
+           _tds$11$textContent === void 0
+             ? void 0
+             : _tds$11$textContent.trim()) || "Hiện chưa có",
+         total:
+           ((_tds$12$textContent = tds[12].textContent) === null ||
+           _tds$12$textContent === void 0
+             ? void 0
+             : _tds$12$textContent.trim()) || "Hiện chưa có",
+         letter_grades:
+           ((_tds$13$textContent = tds[13].textContent) === null ||
+           _tds$13$textContent === void 0
+             ? void 0
+             : _tds$13$textContent.trim()) || "Hiện chưa có",
+       };
+     });
+     return data;
+   });
     let text = "";
     for (const data of listDiemThiData) {
       text += `Môn: *${data.module}*\nLần học thứ: *${data.number_of_times_retaken}*\nĐánh giá: *${data.evaluation}*\nChuyên cần: ${data.cc}\nĐiểm thi: ${data.score}\nĐiểm tổng kết: *${data.overall_subject_grade}*\nĐiểm chữ: *${data.letter_grades}*\nSố tín chỉ: *${data.credit}*\n\n`;
